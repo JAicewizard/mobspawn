f5f0dfca8ea0d44c632745dff55feb2a9c253c64
jaap aarts <jaap.aarts1@gmail.com>
2016-05-08 12:19:38 +0200

    second version

--- src/pw/JA/mobspawn/mobspawn.java
+++ src/pw/JA/mobspawn/mobspawn.java
@@ -1,6 +1,8 @@
 package pw.JA.mobspawn;
 
-
+import org.bukkit.plugin.command.Command;
+import org.bukkit.plugin.command.CommandSender;
+import org.bukkit.plugin.enteity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class mobspawn extends JavaPlugin{
@@ -8,5 +10,18 @@ public class mobspawn extends JavaPlugin{
         getLogger().info("getlogger at its best");
         System.out.println("and the clasic ones");
     }
+    @Overwrite
+    public boolean onCommand(commandSender sender, Command cmd, String Label, string[] args){
+        if (cmd.getName().equalsIgnoreCase("MS")){
+            if (sender instanceof Player){
+                Player player = (Player)  sender;
+                String name = sender.getName();
+                player.sendMessage("hey you are "+name+"aren't you?");
+            }else{
+                sender.sendMessage("NO ONLY PLAYERS GRRR");
+            }
+        }
 
+        return false;
+    }
 }
