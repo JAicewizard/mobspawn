8043ae491d8b114a28bfe6d3df9b85c36ca33471
JAicewizard <jaap.aarts1@gmail.com>
2016-05-08 11:03:56 +0200

    this is still a testing bould

---
+++ src/pw/JA/mobspawn/mobspawn.java
@@ -0,0 +1,12 @@
+package pw.JA.mobspawn;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class mobspawn extends JavaPlugin{
+    public void onEnable(){
+        getLogger().info("getlogger at its best");
+        System.out.println("and the clasic ones");
+    }
+
+}
