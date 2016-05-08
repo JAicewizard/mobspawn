f89d46ec0f2399ca7592e532c4a75ef8f327704f
jaap aarts <jaap.aarts1@gmail.com>
2016-05-08 15:25:21 +0200

    now support both windows and unix filepaths

--- src/pw/JA/mobspawn/mobspawn.java
+++ src/pw/JA/mobspawn/mobspawn.java
@@ -1,5 +1,6 @@
 package pw.JA.mobspawn;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -15,8 +16,20 @@ public class mobspawn extends JavaPlugin{
         if (cmd.getName().equalsIgnoreCase("AMS")){
             if (sender instanceof Player){
                 Player player = (Player)  sender;
+                if(player.hasPermission("mobspawn.mod")){
+                    player.sendMessage(ChatColor.AQUA+"you have the correct permissions");
+                }else{
+                    player.sendMessage(ChatColor.RED+"you do not have the correct permissions");
+                }
+            }else{
+                sender.sendMessage("NO ONLY PLAYERS GRRRWHAHAH");
+            }
+        }
+        if (cmd.getName().equalsIgnoreCase("test")){
+            if (sender instanceof Player){
+                Player player = (Player)  sender;
                 String name = sender.getName();
-                player.sendMessage("hey you are "+name+"aren't you?");
+                player.sendMessage(ChatColor.AQUA+"hey you are "+name+" aren't you?");
             }else{
                 sender.sendMessage("NO ONLY PLAYERS GRRRWHAHAH");
             }
