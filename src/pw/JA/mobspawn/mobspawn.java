c27144646e4aba442d9641b3efb285718fcd666d
JAicewizard <jaap.aarts1@gmail.com>
2016-05-09 20:42:36 +0200

    added a onjoin thingy

--- src/pw/JA/mobspawn/mobspawn.java
+++ src/pw/JA/mobspawn/mobspawn.java
@@ -4,12 +4,20 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.EventHandler;
 
-public class mobspawn extends JavaPlugin{
+import java.util.HashMap;
+import java.util.UUID;
+
+public class mobspawn extends JavaPlugin implements Listener{
     public void onEnable(){
         getLogger().info("getlogger at its best");
         System.out.println("and the clasic ones");
+        getServer().getPluginManager().registerEvents(this, this);
     }
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String Label, String[] args){
@@ -37,7 +45,32 @@ public class mobspawn extends JavaPlugin{
 
         return false;
     }
-    public void usrPerm(){
+    public void usrPermget(Player player, String permission){
+        HashMap<UUID, PermissionAttachment> perms = new HashMap<UUID, PermissionAttachment>();
+        PermissionAttachment attach = player.addAttachment(this);
+        perms.put(player.getUniqueId(), attach);
+        attach.getPermissions();
+    }
+    public void usrPermset(Player player, String permission){
+        HashMap<UUID, PermissionAttachment> perms = new HashMap<UUID, PermissionAttachment>();
+        PermissionAttachment attach = player.addAttachment(this);
+        perms.put(player.getUniqueId(), attach);
+        attach.setPermission(permission, true);
+    }
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player playerInfo = event.getPlayer();
+        String name = playerInfo.getName();
+        playerInfo.sendMessage(name);
+        boolean TF = name.equals("jaap313");
+        playerInfo.sendMessage(Boolean.toString(TF)+" "+name.length());
+        playerInfo.sendMessage(name);
+        playerInfo.sendMessage("jaap313");
+        if (name.equals("jaap313")) {
+            playerInfo.sendMessage("almost  ");
+            usrPermset( playerInfo, "ams");
+            playerInfo.sendMessage("done");
 
+        }
     }
 }
